54baff76c82b5111cd4d72b3a1b630e98aad684d
tometaro07 <93013897+tometaro07@users.noreply.github.com>
2021-10-23 23:14:56 +0100

    Delete turtlebot3_datasets directory

--- turtlebot3_datasets/scripts/download_dataset.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-gdown -O `rospack find turtlebot3_datasets`/data/slam_easy.bag https://drive.google.com/uc?id=1a6csy_UKncrv7Eha5hWuWMAUSnbeJkWq
-gdown -O `rospack find turtlebot3_datasets`/data/fixed_slam_easy.bag https://drive.google.com/uc?id=186PfQ7CkX9EaTo1Sfuc9SekZ3-MX28M5
